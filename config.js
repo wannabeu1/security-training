2a5a822516c25530dcae740bdbc32d69056f3f85
wannabeu1 <leejinam@gmail.com>
2026-08-27 23:59:34 +0900

    Configure access log collector

---
+++ config.js
@@ -0,0 +1,8 @@
+window.TRAINING_CONFIG = {
+  // Google Apps Script를 웹 앱으로 배포한 뒤 생성된 /exec URL로 교체하세요.
+  collectorUrl: 'https://script.google.com/macros/s/AKfycbwebLjfGpgA3y4walIGdi0aiJXIq1dGfTGWR0iLW45W6iuniAHzxY758SvrcY4jGBle/exec',
+  // 공인 IPv4/IPv6 조회 주소입니다. 사내 IP 조회 API가 있다면 해당 주소로 교체하세요.
+  // IP 수집을 원하지 않으면 빈 문자열로 설정할 수 있습니다.
+  ipLookupUrl: 'https://api64.ipify.org?format=json',
+  campaign: 'server-recovery-qr-2026-01'
+};
